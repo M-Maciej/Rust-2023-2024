aa36f048f3ace94837c6618d579f565d1f9880e0
M-Maciej <130112810+M-Maciej@users.noreply.github.com>
2023-12-21 23:20:02 +0100

    chapter 2 finished.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,28 +1,52 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Give me a number from 1 to 10");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
-    match io::stdin().read_line(&mut guess) {
-        Ok(n) => {
-            println!("{n} bytes read");
-            println!("{guess}");
-            if guess.ends_with("\r\n") {
-                println!("Windows-style newline found (\\r\\n)");
+    println!("The secret number is: {secret_number}");
+
+    let mut guess = String::new();
+    loop {
+        guess.clear();
+        match io::stdin().read_line(&mut guess) {
+            Ok(n) => {
+                println!("{n} bytes read");
+                println!("{guess}");
+                if guess.ends_with("\r\n") {
+                    println!("Windows-style newline found (\\r\\n)");
+                }
+                // Check for Unix-style newline
+                else if guess.ends_with("\n") {
+                    println!("Unix-style newline found (\\n)");
+                }
+                // No newline found
+                else {
+                    println!("No newline found");
+                }
             }
-            // Check for Unix-style newline
-            else if guess.ends_with("\n") {
-                println!("Unix-style newline found (\\n)");
+            Err(error) => println!("error: {error}"),
+        }
+
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number");
+                continue;
             }
-            // No newline found
-            else {
-                println!("No newline found");
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
             }
         }
-        Err(error) => println!("error: {error}"),
-    }
 
-    println!("You have entered {}", guess.trim());
+        println!("You have entered {}", guess);
+    }
 }
